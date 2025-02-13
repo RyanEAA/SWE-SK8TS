91f596afe3ecaa7b3856233b6187930e8c4481ef
John V <jvazqu14@stedwards.edu>
2025-02-13 14:52:34 -0600

    Tried adding api call to the catelog page

--- src/Pages/Catelog.jsx
+++ src/Pages/Catelog.jsx
@@ -1,21 +1,33 @@
-import {Link} from 'react-router-dom'
-import '../css/ProductBox.css'
-import ProductBox from '../Components/ProductBox'
-import '../css/Catelog.css'
-import { PRODUCTS } from '../Products'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import '../css/ProductBox.css';
+import ProductBox from '../Components/ProductBox';
+import '../css/Catelog.css';
+
 function Catelog() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch products from the API
+    fetch('http://localhost:5000/api/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error('Error fetching products:', error));
+  }, []);
 
   return (
     <div className='product-gallery'>
-      {PRODUCTS.products.map((product) => (
-        <ProductBox id={product.id} name={product.name} price={product.price} photo={product.image}/>
+      {products.map((product) => (
+        <ProductBox
+          key={product.id}
+          id={product.id}
+          name={product.name}
+          price={product.price}
+          photo={product.image}
+        />
       ))}
-      {/* <ProductBox id={PRODUCTS[1].id} name={PRODUCTS[1].name} price={PRODUCTS[1].price} photo={PRODUCTS[1].image}/>
-      <ProductBox id={PRODUCTS[2].id} name={PRODUCTS[2].name} price={PRODUCTS[2].price} photo={PRODUCTS[2].image}/>
-      <ProductBox id={PRODUCTS[3].id} name={PRODUCTS[3].name} price={PRODUCTS[3].price} photo={PRODUCTS[3].image}/>
-      <ProductBox id={PRODUCTS[4].id} name={PRODUCTS[4].name} price={PRODUCTS[4].price} photo={PRODUCTS[4].image}/> */}
     </div>
-  )
+  );
 }
 
-export default Catelog
+export default Catelog;
