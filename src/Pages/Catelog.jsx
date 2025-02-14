46a50e1c674fe2000937ae3afe64e9c37b45a864
John V <jvazqu14@stedwards.edu>
2025-02-13 20:17:22 -0600

    added api call

--- src/Pages/Catelog.jsx
+++ src/Pages/Catelog.jsx
@@ -1,39 +1,8 @@
-// import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
-// import '../css/ProductBox.css';
-// import ProductBox from '../Components/ProductBox';
-// import '../css/Catelog.css';
-
-// function Catelog() {
-//   const [products, setProducts] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch products from the API
-//     fetch('http://167.71.25.102:3636/products')
-//       .then((response) => response.json())
-//       .then((data) => setProducts(data))
-//       .catch((error) => console.error('Error fetching products:', error));
-//   }, []);
-//   console.log('App component rendered');
-//   return (
-//     <div className='product-gallery'>
-//       {products.map((product) => (
-//         <ProductBox
-//           key={product.id}
-//           id={product.id}
-//           name={product.name}
-//           price={product.price}
-//           photo={product.image}
-          
-//         />
-//       ))}
-//     </div>
-//   );
-
-// }
-
-// export default Catelog;
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import '../css/ProductBox.css';
+import ProductBox from '../Components/ProductBox';
+import '../css/Catelog.css';
 
 function Catelog() {
   return (
