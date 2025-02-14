10fdab5cf3d7c5586b902c241dc97993488e9cec
John V <jvazqu14@stedwards.edu>
2025-02-13 20:17:29 -0600

    added api call

--- src/Pages/Catelog.jsx
+++ src/Pages/Catelog.jsx
@@ -5,12 +5,31 @@ import ProductBox from '../Components/ProductBox';
 import '../css/Catelog.css';
 
 function Catelog() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch products from the API
+    fetch('http://167.71.25.102:3636/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error('Error fetching products:', error));
+  }, []);
+  console.log('App component rendered');
   return (
-    <div>
-      <h1>Catalog Page</h1>
-      <p>This is the catalog page.</p>
+    <div className='product-gallery'>
+      {products.map((product) => (
+        <ProductBox
+          key={product.id}
+          id={product.id}
+          name={product.name}
+          price={product.price}
+          photo={product.image}
+          
+        />
+      ))}
     </div>
   );
+
 }
 
 export default Catelog;
