c85dba8bbf443633388530cc3f130b7eae4be1f5
eligulley1 <eligulley@gmail.com>
2025-02-24 14:02:42 -0600

    added lines to vite.config.js to ensure dev container starts on port 3000

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,  // Set the dev server port to 3000
+    host: true   // Ensure it binds correctly inside the container
+  },
   build: {
     target: 'esnext', // more modern target
-  },
+  }
 });
 
