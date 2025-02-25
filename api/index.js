949b126c1f374cc11d43227f51e48290e5628b1b
eligulley1 <eligulley@gmail.com>
2025-02-25 13:14:28 -0600

    fixed products db name in index.js for api

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ function handleDisconnect() {
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB
+    database: process.env.MYSQL_PRODUCTS_DB
   });
 
   userDb = mysql.createConnection({
