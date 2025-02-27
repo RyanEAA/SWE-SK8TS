c6cf070edbb68c64f6928397fc5eb91f23132710
Wilsonf8 <Wilsonflores880@gmail.com>
2025-02-26 18:25:42 -0600

    Cart Items photo fix

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -2,12 +2,11 @@ import '../css/CartItem.css';
 import React from 'react';
 
 function CartItem(props) {
-    console.log(props.item);
 
     return (
         <div className='container-container'>
             <div className="item-container">
-                <img className='cart-item-image' src={props.item.image_url} alt={props.item.name} />
+                <img className='cart-item-image' src={`./Images/products/${props.item.image_path}`} alt={props.item.name} />
                 <div>{props.item.name}</div>
                 <div>${(props.item.price * props.item.qty).toFixed(2)}</div>
                 <div>
