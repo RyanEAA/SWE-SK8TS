7fa8907ebfe9921d97c0fa0549c2930eeb560038
Wilsonf8 <Wilsonflores880@gmail.com>
2025-02-28 01:56:24 -0600

    cart imports 2nd try

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import CartItem from "../Components/CartItem.jsx";
 import '../css/Cart.css';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 function Cart({ cartItems, onAdd, onRemove }) {
   const itemsPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0);
