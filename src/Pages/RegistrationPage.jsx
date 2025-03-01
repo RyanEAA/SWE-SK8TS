306a058cecd29063ae513afd173e793d9a25ce10
RyanEAA <aparicioryan2003@gmail.com>
2025-02-28 23:20:51 -0600

    added styling to register page

--- src/Pages/RegistrationPage.jsx
+++ src/Pages/RegistrationPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
-
+import '../css/RegistrationPage.css';
 
 const RegistrationPage = () => {
   // for navigating to other pages
