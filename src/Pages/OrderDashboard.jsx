8a294513b342defeb2165de0814a7681f88d67b7
Wilsonf8 <Wilsonflores880@gmail.com>
2025-03-27 13:38:58 -0500

    order dashboard reverse

--- src/Pages/OrderDashboard.jsx
+++ src/Pages/OrderDashboard.jsx
@@ -67,7 +67,7 @@ function OrderDashboard() {
                 <div className='unclaimed-order-price'>Order Date</div>
                 <div></div>
             </div>
-            {[...unclaimedOrders].reverse().map(order => (
+            {[...unclaimedOrders].map(order => (
                 <div className='unclaimed-order-container'>
                     <div className='unclaimed-order-id'>{order.order_id} </div>
                     <div className='unclaimed-order-user-id'>{order.user_id}</div>
