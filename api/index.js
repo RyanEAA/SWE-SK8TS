920b9bc1282fcb4154c13a32c414730e1c5fa9e6
eligulley1 <eligulley@gmail.com>
2025-04-13 20:27:20 -0500

    changed /api/chat to /chat, so dumb

--- api/index.js
+++ api/index.js
@@ -430,7 +430,7 @@ app.delete('/products/:id', (req, res) => {
   });
 });
 
-app.post("/api/chat", async (req, res) => {
+app.post("/chat", async (req, res) => {
   const userMessage = req.body.message;
 
   try {
