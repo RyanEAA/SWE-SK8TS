74f803b66b263a6d2838b64ee8d0293f21494db5
RyanEAA <aparicioryan2003@gmail.com>
2025-04-15 00:43:14 -0500

    fixed claimed orders view for admin by fixing api call from /user to /employee

--- src/Components/AdminComponents/ClaimedOrders.jsx
+++ src/Components/AdminComponents/ClaimedOrders.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import Order from '../Order';
+import Cookies from 'js-cookie';
+import '../../css/admin/Admin.css';
 
 function ClaimedOrders() {
     const [isLoading, setIsLoading] = useState(true);
     const [claimedOrders, setClaimedOrders] = useState([]);
+    const userId = Cookies.get('user_id');
+
 
     useEffect(() => {
         const fetchClaimedOrders = async () => {
             try {
                 setIsLoading(true);
-                const response = await fetch(`https://sk8ts-shop.com/api/orders/user/${userId}`);
+                const response = await fetch(`https://sk8ts-shop.com/api/orders/employee/${userId}`);
                 const data = await response.json();
                 setClaimedOrders(data);
             } catch (error) {
@@ -30,7 +34,7 @@ function ClaimedOrders() {
         {isLoading ? (
             <div className="loading-spinner">Loading...</div>
         ) : (
-            <div className="claimed-orders-list">
+            <div className="claimed-orders-list scrollable-list">
                 {claimedOrders.length > 0 ? (
                     claimedOrders.map(order => (
                         <div 
