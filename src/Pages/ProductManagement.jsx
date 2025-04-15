8583b7a6a16a21f92f87fcaa67776f9ecf47afc0
John V <jvazqu14@stedwards.edu>
2025-04-15 12:00:34 -0500

    Fixed some styling issues

--- src/Pages/ProductManagement.jsx
+++ src/Pages/ProductManagement.jsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 import '../css/ProductManagement.css';
 import '../css/admin/Admin.css';
 import CreateProductPopup from '../Components/AdminComponents/PopUps/CreateProductPopup';
+import '../css/buttons.css';
 
 function ProductManagement() {
   const [products, setProducts] = useState([]);
