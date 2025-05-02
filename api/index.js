fafee5d4efdbaea415c749feb79823b742238da4
eligulley1 <eligulley@gmail.com>
2025-05-02 17:06:48 -0500

    remove path redeclaration

--- api/index.js
+++ api/index.js
@@ -466,7 +466,6 @@ app.post('/products', (req, res) => {
 // Ensure the 'public/Images' directory exists
 const fs = require('fs');
 const multer = require('multer');
-const path = require('path');
 
 // Ensure the 'public/Images' directory exists
 const uploadDir = path.join(__dirname, 'public', 'Images');
